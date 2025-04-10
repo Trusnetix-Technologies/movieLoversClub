dda37bd2b8a8370632fcf9db2dcafb7e4c7c8649
ShubhasmitMalakar <55288337+ShubhasmitMalakar@users.noreply.github.com>
2025-04-10 17:34:38 +0530

    Quick Error Fixing

--- app/lib/constants/address_constants.dart
+++ app/lib/constants/address_constants.dart
@@ -1,11 +1,11 @@
 /**
  * How to get host IP
  * 
- * 1. Open cmd
- * 2. Run ipconfig
+ * 1. Open cmd/terminal
+ * 2. Run ipconfig/ifconfig
  * 3. Look for IPv4 Address
  * 4. Copy the IP Address
  * 5. Replace the localhost with the IP Address
  * 6. and replace the port with the port number
  */
-const host = "http://192.168.29.86:5001";
+const host = "http://192.168.29.217:5001";
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
   // ----- OTP FUNCTIONS START ----
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // Limit each IP to 10 requests per windowMs
+    max: 100, // Limit each IP to 10 requests per windowMs
     message: "Too many requests, please try again later.",
   });
   // GENERATE AND SEND OTP
